48056cadb7b236d45cc68f751993628d9d8c5c71
Bomashi Ndeke Ezeckiel <ezeckielbomashi@gmail.com>
2025-11-09 13:29:34 +0100

    Update Iris TP.py

--- Iris TP.py
+++ Iris TP.py
@@ -10,3 +10,7 @@ print(df.isnull().sum())
 
 #Voir les Statistiques
 print(df.describe())
+
+#Ici on visualiser la longueur et largeur des sépales en fonction des espèces 
+sns.scatterplot(data=df, x='sepal_length', y='sepal_width', hue='species')
+plt.show()
